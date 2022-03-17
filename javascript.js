0190478a639e6be076d07e0e5b5365a97c9247c2
jrhei <jannorheydon@gmail.com>
2022-03-17 09:56:00 +0800

    BETA 0.02 * Add function that starts a round that determines who is the winner   based on the values of the computer and the user * Add conditional statements which determines the expected game outcome when player chooses:   rock, paper or scissors.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
 function computerPlay(){
+    //give designated values from 1-3 to rock, papers and scissors
     let rock = 1;
     let paper = 2;
     let scissors = 3;
+
+    //randomize a number from 1-3 and save it to outcome variable
     outcome = Math.floor(Math.random()*3) + 1;
+
+    /*conditional statements that return rock paper or scissor
+    based on the outcome*/
     if (outcome === rock){
         return "rock";
     }
@@ -16,3 +22,58 @@ function computerPlay(){
         return (outcome + "error");
     }
 }
+//function that starts the round
+function startGame(playerSelection, computerSelection){
+    //compare value of playerSelection and computerSelection
+    //determine the outcome of the winner based on rock-paper-scissor rules
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === 'rock'){
+        if (computerSelection === 'paper'){
+            return "You lose! Paper beats Rock";
+        }
+        else if (computerSelection === 'scissors'){
+            return "You win! Rock beats scissors";
+        }
+        else if (computerSelection === 'rock'){
+            return "It's a draw! Rock cant beat rock";
+        }
+        //returns the values of computer and player if both had invalid values for error debugging
+        else{
+            return "Error-> \nComputer: " + computerSelection + "\nPlayer: " + playerSelection;
+        }
+    }
+    else if (playerSelection === 'paper'){
+        if (computerSelection === 'paper'){
+            return "It's a draw! Paper cant beat Paper";
+        }
+        else if (computerSelection === 'scissors'){
+            return "You lose! scissors beats Paper";
+        }
+        else if (computerSelection === 'rock'){
+            return "You win! Paper beats Rock";
+        }
+        //returns the values of computer and player if both had invalid values for error debugging
+        else{
+            return "Error-> \nComputer: " + computerSelection + "\nPlayer: " + playerSelection;
+        }
+    }
+    else if (playerSelection === 'scissors'){
+        if (computerSelection === 'paper'){
+            return "You win! scissors beats Paper";
+        }
+        else if (computerSelection === 'scissors'){
+            return "It's a draw! scissors cant beat scissors";
+        }
+        else if (computerSelection === 'rock'){
+            return "You lose! rock beats scissors";
+        }
+        //returns the values of computer and player if both had invalid values for error debugging
+        else{
+            return "Error-> \nComputer: " + computerSelection + "\nPlayer: " + playerSelection;
+        }
+    }
+}
+
+const playerSelection = "roCk";
+const computerSelection = computerPlay();
+console.log(startGame(playerSelection, computerSelection));
